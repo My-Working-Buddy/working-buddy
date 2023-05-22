93f58170273555f156dd1ca57168f47280612291
Felix Ombura <moreenkim36@gmail.com>
2023-05-22 23:40:38 +0300

    Create pages

---
+++ src/app/pages/ContactUs.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Text, Heading } from "@chakra-ui/react";
+
+
+const ContactUs = () => {
+return (
+    <div>
+      <div>
+        <Box maxW="32rem">
+          <Heading px="5" py="5" fontSize="lg">Get in-touch with us</Heading>
+        </Box>
+        <Box>
+          <Text px="5" fontSize="md">Get us on email and phone via 123-456</Text>
+        </Box>
+      </div>
+    </div>
+)
+}
+
+export default ContactUs
