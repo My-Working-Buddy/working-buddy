bd3959d405a119fc3d25e30506fb131d6c039461
Felix Ombura <moreenkim36@gmail.com>
2023-05-22 23:50:55 +0300

    Make routing changes on file location

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AboutUs from "./app/pages/AboutUs";
+import AboutUs from "./app/pages/About/AboutUs";
 import HomePage from "./app/pages/Homepage";
-import ContactUs from "./app/pages/ContactUs"
-import Login from "./app/pages/Login"
+import ContactUs from "./app/pages/ContactUs/ContactUs"
+import Login from "./app/pages/Login/Login"
 
 function Routing() {
   return (
