faf97d296ceb08e0fbfd86bcf3db0112033bdad2
Felix Ombura <moreenkim36@gmail.com>
2023-06-29 00:28:53 +0300

    feat: add the banner component to main file

--- src/app/pages/Homepage.tsx
+++ src/app/pages/Homepage.tsx
@@ -1,10 +1,11 @@
 import { Footer } from '../components/Footer/Footer';
 import Navbar from '../components/Navbar/Navbar';
-
+import Banner from '../components/Banner/Banner';
 export default function HomePage() {
   return (
     <>
       <Navbar />
+      <Banner/>
       <Footer />
     </>
   );
