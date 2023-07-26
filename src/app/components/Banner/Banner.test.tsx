88ddf4ab4b71995af7f3d52f9cd2a9c934661d17
Felix Ombura <moreenkim36@gmail.com>
2023-07-26 13:12:38 +0300

    fix: rename the alt test and reduce banner text in testing

--- src/app/components/Banner/Banner.test.tsx
+++ src/app/components/Banner/Banner.test.tsx
@@ -4,8 +4,8 @@ import Banner from './Banner';
 describe('Banner Component', () => {
   it('ensure alt contains correct value', () => {
     render(<Banner/>)
-    const testIMage = screen.getByAltText(/banner-logo/i)
-  expect(testIMage).toBeInTheDocument()
+    const testAlt = screen.getByAltText(/banner-logo/i)
+  expect(testAlt).toBeInTheDocument()
   })
   
   it('ensure src contains correct value', () => {
@@ -20,7 +20,7 @@ describe("Banner Text", () => {
 
   it('renders banner text', () => {
     render(<Banner/>)
-    const messageTest = screen.getByText(/Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum/i)
+    const messageTest = screen.getByText(/Lorem ipsum dolor sit amet/i)
     expect(messageTest).toBeInTheDocument()
   })
 })
