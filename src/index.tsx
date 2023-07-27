8cf98be943b33da4483cac5b6babc0e91f72aa3e
Felix Ombura <moreenkim36@gmail.com>
2023-07-27 12:28:36 +0300

    feat: carousel- add react slick to create image slider

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 import "@fontsource/fredoka-one/400.css";
 import "@fontsource/abeezee/400.css";
 
