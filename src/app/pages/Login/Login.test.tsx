05a8bd9416e8c7245ccbe4c826c39032a816b3ed
Felix Ombura <moreenkim36@gmail.com>
2023-09-12 14:19:16 +0300

    delete: remove login and signup files replaced by auth0

--- src/app/pages/Login/Login.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import Login from './Login';
-
-
-it('should render correct heading', () => {
-     render(<Login/>);
-     const headingElement = screen.getByText(/login/i);
-     expect(headingElement).toBeInTheDocument();
-})
