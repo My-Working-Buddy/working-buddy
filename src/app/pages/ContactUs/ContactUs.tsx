a7daf1ee2a271813750fe119a15b66f765529325
Felix Ombura <moreenkim36@gmail.com>
2023-09-22 10:19:01 +0300

    feat: create a form for the contact us

--- src/app/pages/ContactUs/ContactUs.tsx
+++ src/app/pages/ContactUs/ContactUs.tsx
@@ -1,20 +1,79 @@
-import React from 'react';
-import { Box, Text, Heading } from "@chakra-ui/react";
+import React from "react";
+import {
+  Button,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  InputGroup,
+  InputLeftAddon,
+  HStack,
+  Textarea,
+  Image
+} from "@chakra-ui/react";
+import mwbCanva from "../../../assets/mwb-canva.jpg";
 
 
 const ContactUs = () => {
-return (
+  return (
     <div>
-      <div>
-        <Box maxW="32rem">
-          <Heading px="5" py="5" fontSize="lg">Get in-touch with us</Heading>
-        </Box>
-        <Box>
-          <Text px="5" fontSize="md">Get us on email and phone via 123-456</Text>
-        </Box>
-      </div>
-    </div>
-)
-}
+      <Stack direction="row">
+      <FormControl px="10" py="5">
+        <Heading size="md">CONTACT US</Heading>
 
-export default ContactUs
+        <HStack py="5">
+          <div>
+            <FormLabel>First name</FormLabel>
+            <Input width="1/2" placeholder="First name" />
+          </div>
+          <div>
+            <FormLabel>Last name</FormLabel>
+            <Input width="1/2" placeholder="Last name" />
+          </div>
+        </HStack>
+        <Stack>
+          <div>
+            <FormLabel>Email</FormLabel>
+            <Input w="1/2" type="Email" py="5" />
+          </div>
+          <div>
+            <FormLabel>Phone</FormLabel>
+
+            <Stack spacing={4}>
+              <InputGroup>
+                <InputLeftAddon children="+254" />
+                <Input
+                  w="1/2"
+                  type="phone"
+                  borderLeftRadius="0"
+                  placeholder="phone number"
+                />
+              </InputGroup>
+            </Stack>
+          </div>
+          <div>
+            <FormLabel>Comment or Message</FormLabel>
+            <Textarea isRequired boxSize="xs" />
+          </div>
+
+          <div>
+          <Button type="submit" colorScheme="teal">Submit</Button>
+        </div>
+        </Stack>
+      
+      </FormControl>
+      <Image
+          objectFit="none"
+          maxW={{ base: "200%", lg: "500px" }}
+          src={mwbCanva}
+          alt="banner-logo"
+        />
+      </Stack>
+  
+ 
+    </div>
+  );
+};
+
+export default ContactUs;
